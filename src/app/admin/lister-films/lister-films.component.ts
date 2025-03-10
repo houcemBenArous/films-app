b648ac029586f36a7af31991d1a63741aff5ca07
houcemBenArous <houcem.benarous2001@gmail.com>
2025-03-10 12:39:57 +0100

    Create FilmsService and integrate it with ListerFilmsComponent for film listing

--- src/app/admin/lister-films/lister-films.component.ts
+++ src/app/admin/lister-films/lister-films.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { Film } from '../../shared/models/film';
+import { FilmsService } from '../../services/films.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lister-films',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './lister-films.component.html',
   styleUrl: './lister-films.component.css'
 })
 export class ListerFilmsComponent {
+  tabFilms: Film[] = [];
+  constructor(private filmsService : FilmsService) { }
+  
+  ngOnInit(): void {
+    this.tabFilms=this.filmsService.getFilms();
+    }
 
+  supprimer(index : number){
+    this.filmsService.delete(index);
+  }
+  confirmAndDelete(index : number){
+    if (confirm("voulez vous supprimer le film ?")){
+      this.supprimer(index);
+    }
+  }
 }
---
+++ src/app/services/films.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Film } from '../shared/models/film';
+import { FILMS } from '../shared/models/des-films';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FilmsService {
+  films : Film[]=FILMS;
+  constructor() { }
+  getFilms () : Film []
+  {
+    return this.films;
+  }
+  delete(index : number){
+    this.films.splice(index,1)
+  }
+}
